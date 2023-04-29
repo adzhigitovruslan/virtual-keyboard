ea2bd0661021329fe2cc6d8697b2a85b2eead410
Ruslan Adzhigitov <adzhigitovruslan@gmail.com>
2023-04-29 21:04:16 +0800

    feat: add basic page layout

--- virtual-keyboard/js/modules/windows-keyboard.js
+++ virtual-keyboard/js/modules/windows-keyboard.js
@@ -1,1 +1,13 @@
-export function windowsKeyboard() {}
+export function windowsKeyboard() {
+
+  const body = document.querySelector("body");
+  const main = document.createElement("main");
+  const keyboard = document.createElement("div");
+
+  main.classList.add("main");
+  keyboard.classList.add("keyboard");
+
+  body.appendChild(main);
+  main.appendChild(keyboard);
+
+}
