288e7c6d12916ebafad4e553ba540f46675d0720
Ruslan Adzhigitov <adzhigitovruslan@gmail.com>
2023-05-02 02:26:25 +0800

    feat: add switching language && uppercase to ru letters while pressing shift

--- virtual-keyboard/js/modules/keyboard.js
+++ virtual-keyboard/js/modules/keyboard.js
@@ -1,7 +1,7 @@
 import { Button } from "./button.js";
 
 const eventCodeEng = {
-  "Backquote": "§",
+  "Backquote": { "§": "±" },
   "Digit1": { 1: "!" },
   "Digit2": { 2: "@" },
   "Digit3": { 3: "#" },
@@ -66,14 +66,81 @@ const eventCodeEng = {
   "ArrowDown": "",
   "ArrowRight": "",
 };
+const eventCodeRu = {
+  "Backquote": { ">": "<" },
+  "Digit1": { 1: "!" },
+  "Digit2": { 2: "\"" },
+  "Digit3": { 3: "№" },
+  "Digit4": { 4: "%" },
+  "Digit5": { 5: ":" },
+  "Digit6": { 6: "," },
+  "Digit7": { 7: "." },
+  "Digit8": { 8: ";" },
+  "Digit9": { 9: "(" },
+  "Digit0": { 0: ")" },
+  "Minus": { "-": "_" },
+  "Equal": { "=": "+" },
+  "Backspace": "Backspace",
+  "Tab": "Tab",
+  "KeyQ": { "й": "Й" },
+  "KeyW": { "ц": "Ц" },
+  "KeyE": { "у": "У" },
+  "KeyR": { "к": "К" },
+  "KeyT": { "е": "Е" },
+  "KeyY": { "н": "Н" },
+  "KeyU": { "г": "Г" },
+  "KeyI": { "ш": "Ш" },
+  "KeyO": { "щ": "Щ" },
+  "KeyP": { "з": "З" },
+  "BracketLeft": { "х": "Х" },
+  "BracketRight": { "ъ": "Ъ" },
+  "Enter": "Enter",
+  "CapsLock": "Capslock",
+  "KeyA": { "ф": "Ф" },
+  "KeyS": { "ы": "Ы" },
+  "KeyD": { "в": "В" },
+  "KeyF": { "а": "А" },
+  "KeyG": { "п": "П" },
+  "KeyH": { "р": "Р" },
+  "KeyJ": { "о": "О" },
+  "KeyK": { "л": "Л" },
+  "KeyL": { "д": "Д" },
+  "Semicolon": { "ж": "Ж" },
+  "Quote": { "э": "Э" },
+  "Backslash": { "ё": "Ё" },
+  "ShiftLeft": "Shift",
+  "IntlBackslash": { "]": "[" },
+  "KeyZ": { "я": "Я" },
+  "KeyX": { "ч": "Ч" },
+  "KeyC": { "с": "С" },
+  "KeyV": { "м": "М" },
+  "KeyB": { "и": "И" },
+  "KeyN": { "т": "Т" },
+  "KeyM": { "ь": "Ь" },
+  "Comma": { "б": "Б" },
+  "Period": { "ю": "Ю" },
+  "Slash": { "/": "?" },
+  "ArrowUp": "",
+  "ShiftRight": "Shift",
+  "ControlLeft": "Control",
+  "AltLeft": "Alt",
+  "MetaLeft": "Meta",
+  "Space": "space",
+  "MetaRight": "Meta",
+  "AltRight": "Alt",
+  "ArrowLeft": "",
+  "ArrowDown": "",
+  "ArrowRight": "",
+};
 
 export class Keyboard {
   constructor(options) {
     this.mainClass = options.mainClass;
     this.wrapperClass = options.wrapperClass;
+    this.switch = options.language;
   }
 
-  displayKeyboard() {
+  createKeyboard() {
     const body = document.querySelector("body");
     const main = document.createElement("main");
     const keyboardWrapper = document.createElement("div");
@@ -83,53 +150,56 @@ export class Keyboard {
 
     body.appendChild(main);
     main.appendChild(keyboardWrapper);
+  }
 
-    for (let key in eventCodeEng) {
-      if (typeof eventCodeEng[key] === "object") {
+  displayKeyboard(language) {
+    this.switch = language === "eng" ? eventCodeEng : language === "ru" ? eventCodeRu : undefined;
+
+    for (let key in this.switch) {
+      if (typeof this.switch[key] === "object") {
         const button = new Button({
           eventCode: key,
           className: "row__key",
-          innerText: Object.keys(eventCodeEng[key]),
+          innerText: Object.keys(this.switch[key]),
         });
-        keyboardWrapper.append(button.makeButtons());
+        document.querySelector(`.${this.wrapperClass}`).append(button.makeButtons());
 
-      } else if (typeof eventCodeEng[key] !== "object") {
+      } else if (typeof this.switch[key] !== "object") {
 
         const button = new Button({
           eventCode: key,
           className: "row__key",
-          innerText: eventCodeEng[key],
+          innerText: this.switch[key],
         });
 
-        keyboardWrapper.append(button.makeButtons());
+        document.querySelector(`.${this.wrapperClass}`).append(button.makeButtons());
       }
     }
-
   }
 
   shiftLetters() {
-
     document.addEventListener("keydown", (event) => {
+
       if (event.code === "ShiftLeft") {
-        document.querySelector(`.${this.wrapperClass}`).innerHTML = "";
+        this.toClearScreen();
 
-        for (let key in eventCodeEng) {
+        for (let key in this.switch) {
 
-          if (typeof eventCodeEng[key] === "object") {
+          if (typeof this.switch[key] === "object") {
 
             const button = new Button({
               eventCode: key,
               className: "row__key",
-              innerText: Object.values(eventCodeEng[key]),
+              innerText: Object.values(this.switch[key]),
             });
 
             document.querySelector(`.${this.wrapperClass}`).append(button.makeButtons());
-          } else if (typeof eventCodeEng[key] !== "object") {
+          } else if (typeof this.switch[key] !== "object") {
 
             const button = new Button({
               eventCode: key,
               className: "row__key",
-              innerText: eventCodeEng[key],
+              innerText: this.switch[key],
             });
 
             document.querySelector(`.${this.wrapperClass}`).append(button.makeButtons());
@@ -139,23 +209,25 @@ export class Keyboard {
     });
     document.addEventListener("keyup", (event) => {
       if (event.code === "ShiftLeft") {
-        document.querySelector(`.${this.wrapperClass}`).innerHTML = "";
-        for (let key in eventCodeEng) {
+        
+        this.toClearScreen();
 
-          if (typeof eventCodeEng[key] === "object") {
+        for (let key in this.switch) {
+
+          if (typeof this.switch[key] === "object") {
             const button = new Button({
               eventCode: key,
               className: "row__key",
-              innerText: Object.keys(eventCodeEng[key]),
+              innerText: Object.keys(this.switch[key]),
             });
             document.querySelector(`.${this.wrapperClass}`).append(button.makeButtons());
 
-          } else if (typeof eventCodeEng[key] !== "object") {
+          } else if (typeof this.switch[key] !== "object") {
 
             const button = new Button({
               eventCode: key,
               className: "row__key",
-              innerText: eventCodeEng[key],
+              innerText: this.switch[key],
             });
 
             document.querySelector(`.${this.wrapperClass}`).append(button.makeButtons());
@@ -165,4 +237,7 @@ export class Keyboard {
     });
   }
 
+  toClearScreen() {
+    document.querySelector(`.${this.wrapperClass}`).innerHTML = "";
+  }
 }
--- virtual-keyboard/js/script.js
+++ virtual-keyboard/js/script.js
@@ -1,3 +1,41 @@
-import {windowsKeyboard} from "./modules/windows-keyboard.js";
+import { Keyboard } from "./modules/keyboard.js";
+import { Textarea } from "./modules/textarea.js";
 
-windowsKeyboard();
+let keyPressed = {};
+let language = "eng";
+
+const keyboard = new Keyboard({
+  mainClass: "main",
+  wrapperClass: "keyboard",
+});
+
+const textarea = new Textarea({
+  className: "textarea"
+});
+
+keyboard.createKeyboard();
+keyboard.displayKeyboard(language);
+textarea.displayTextarea();
+keyboard.shiftLetters(language);
+
+function keydownFunc(event) {
+  keyPressed[event.code] = true;
+  if (keyPressed["ShiftLeft"] && keyPressed["AltLeft"]) {
+    console.log(keyPressed);
+    language = language === "eng" ? "ru" : "eng";
+    console.log(language, "first");
+    keyboard.toClearScreen();
+    keyboard.displayKeyboard(language);
+    console.log(language);
+  }
+}
+
+const keyupFunc = (event) => {
+  delete keyPressed[event.code];
+};
+
+document.addEventListener("keydown", keydownFunc);
+document.addEventListener("keyup", keyupFunc);
+
+window.removeEventListener("beforeunload", keydownFunc);
+window.removeEventListener("beforeunload", keyupFunc);
